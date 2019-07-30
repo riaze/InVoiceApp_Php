4c4a2ba9b36d4a25eff292690fd1b1542e722659
riaze <ria.thas20@gmail.com>
2019-07-30 17:38:06 +0200

    configuration is fishied

---
+++ README.txt
@@ -0,0 +1,17 @@
+# InVoice App
+
+-Requirements
+
+-Installations
+
+    -Creating Docker Machines(Otional)
+    -Docker Machines create InVoice Container
+    -Docker version 19.03.0
+    -docker-compose version 1.22.0
+    -Create container (''docker-compose up -d)
+    -Move to /var/www/html ('docker exec -it invoiceapp_php_web_1 bash')
+    -create image ('docker-compose up -d --build')
+
+
+
+
---
+++ public/views/templates/base.php
@@ -0,0 +1,21 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<div id="page">
+    <header>
+        <nav>
+        <?php include_once 'primary-menu.php'; ?>
+        </nav>
+    </header> 
+    <section id="page-content"></section>
+</div>
+    
+</body>
+</html>
